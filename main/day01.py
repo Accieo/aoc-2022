4db2e048dca2378854be95f3f89c19c1f8b56712
Accieo <42718776+Accieo@users.noreply.github.com>
2022-12-01 14:57:34 +0100

    Solution day 1

---
+++ main/day01.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass, field
+
+@dataclass
+class Elf:
+    inventory: list = field(default_factory=list)
+    calories: int = 0
+
+with open('input/day01.txt', 'r') as file:
+    data = file.readlines()
+    data = list(map(lambda x: x.strip(), data))
+
+def common():
+    elf = Elf()
+    elves = list()
+    for n, item in enumerate(data):
+        if item:
+            elf.inventory.append(int(item))
+            elf.calories += int(item)
+            if n == len(data) - 1:
+                elves.append(elf)
+        else:
+            elves.append(elf)
+            elf = Elf()
+
+    return elves
+
+def part_one():
+    elves = common()
+    return max(elves, key=lambda x: x.calories)
+
+def part_two():
+    elves = common()
+    elves.sort(key=lambda x: x.calories, reverse=True)
+    return sum([elf.calories for elf in elves[0:3]])
+
+if __name__ == '__main__':
+    print(part_one().calories)
+    print(part_two())

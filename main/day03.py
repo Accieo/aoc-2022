863b273e7cee49d602e74673fa09d86070fd63bd
Accieo <42718776+Accieo@users.noreply.github.com>
2022-12-03 08:58:32 +0100

    Solution day 3

--- main/day03.py
+++ main/day03.py
@@ -3,23 +3,20 @@ from dataclasses import dataclass
 
 @dataclass
 class Rucksack:
-    cmp_one: str = ''
-    cmp_two: str = ''
+    items: str = ''
 
 prio_lowercase = { letter:prio for letter, prio in zip(ascii_lowercase, range(1,27)) }
 prio_uppercase = { letter:prio for letter, prio in zip(ascii_uppercase, range(27,53)) }
 prio_list = { **prio_lowercase, **prio_uppercase }
 
-with open('examples/day03.txt', 'r') as file:
+with open('input/day03.txt', 'r') as file:
     data = file.read().splitlines()
 
 def common():
     rucksack = Rucksack()
     rucksacks = list()
     for items in data:
-        items_amount = len(items)
-        rucksack.cmp_one = items[:items_amount//2]
-        rucksack.cmp_two = items[items_amount//2:]
+        rucksack.items = items
         rucksacks.append(rucksack)
         rucksack = Rucksack()
 
@@ -29,15 +26,23 @@ def part_one():
     rucksacks = common()
     priority_sum = 0
     for rucksack in rucksacks:
-        cmp_one = set(rucksack.cmp_one)
-        cmp_two = set(rucksack.cmp_two)
+        items_amount = len(rucksack.items)
+        cmp_one = set(rucksack.items[:items_amount//2])
+        cmp_two = set(rucksack.items[items_amount//2:])
         intersection = cmp_one.intersection(cmp_two)
         priority_sum += prio_list[list(intersection)[0]]
 
     return priority_sum
 
 def part_two():
-    return
+    rucksacks = common()
+    priority_sum = 0
+    for i, j in zip(range(0, len(rucksacks) + 1, 3), range(3, len(rucksacks) + 1, 3)):
+        elf_one, elf_two, elf_three = rucksacks[i:j]
+        intersection = set(elf_one.items).intersection(elf_two.items, elf_three.items)
+        priority_sum += prio_list[list(intersection)[0]]
+
+    return priority_sum
 
 if __name__ == '__main__':
     print(part_one())
